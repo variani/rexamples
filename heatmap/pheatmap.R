9104935f34ee8cf5191fe6b3dd945320aec5e228
Your Name <machovariani@gmail.com>
2012-01-24 14:19:01 +0100

    pheatmap

--- heatmap/pheatmap.R
+++ heatmap/pheatmap.R
@@ -15,5 +15,6 @@ test[11:20, seq(2, 10, 2)] <- test[11:20, seq(2, 10, 2)] + 2
 colnames(test) <- paste("Test", 1:10, sep = "")
 rownames(test) <- paste("Gene", 1:20, sep = "")
 
+# heatmap with some cleaning
 pheatmap(test, legend=TRUE, annotation_legend = TRUE, border_color = "black", 
   cluster_row=FALSE, cluster_col=FALSE, fontsize=7, axis=TRUE)
