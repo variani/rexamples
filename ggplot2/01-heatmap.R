bb899689a489864868f1f5e047a6e4c78f7d86ca
variani <machovariani@gmail.com>
2012-10-29 17:24:02 +0100

    added ggplot2 readme

--- ggplot2/01-heatmap.R
+++ ggplot2/01-heatmap.R
@@ -10,12 +10,11 @@ qplot(Sepal.Length, Petal.Length, data = iris, color = Species, size = Petal.Wid
 
 summary(iris)
 
-# - `as.character` is used to get rid of factors (latern on it is easier to add new lables)
 # - for some cases it is needed `include.lowest = TRUE`
 iris <- mutate(iris,
-  Sepal.Length = as.character(cut(Sepal.Length, seq(4, 8, 0.5))),
-  Petal.Length = as.character(cut(Petal.Length, seq(1, 7, 0.5), include.lowest = TRUE)),
-  Petal.Width = as.character(cut(Petal.Width, seq(0, 3, 0.5))))
+  Sepal.Length = cut(Sepal.Length, seq(4, 8, 0.5)),
+  Petal.Length = cut(Petal.Length, seq(1, 7, 0.5), include.lowest = TRUE),
+  Petal.Width = cut(Petal.Width, seq(0, 3, 0.5)))
 
 qplot(Sepal.Length, Petal.Length, data = iris, color = Species, size = Petal.Width)
 
@@ -26,5 +25,12 @@ last_plot() + facet_wrap(~ Species)
 
 ### switch to a smaller dataset
 versicolor <- subset(iris, Species == "versicolor")
-
+versicolor <- mutate(versicolor, 
+  Petal.Width = factor(Petal.Width))
+  
 ggplot(versicolor, aes(Sepal.Length, Petal.Length, fill = Petal.Width)) + geom_tile()
+
+### 
+last_plot() + scale_fill_brewer(palette="Set1") #+ 
+#  scale_x_discrete(expand = c(0, 0)) + scale_y_discrete(expand = c(0, 0))
+#  opts(axis.ticks = theme_blank(), axis.text.x = theme_text(angle = 90, hjust = 0, colour = "grey50"))  
---
+++ ggplot2/readme.md
@@ -0,0 +1,8 @@
+# ggplot2
+
+## 01-heatmap.R
+
+Open topics
+* Color for NA-values. 
+  * When fill-value is continuous, the NA-color is a gray color (assigned automatically by some defaults).
+  * When fill-value is discrete, the NA-color is just transparent.
