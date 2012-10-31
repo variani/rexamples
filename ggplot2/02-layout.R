cf67f39bec006748f077e72c14ac3de26473805f
variani <machovariani@gmail.com>
2012-10-31 10:26:21 +0100

    updated ggplot2

---
+++ ggplot2/02-layout.R
@@ -0,0 +1,9 @@
+# @ http://www.imachordata.com/?p=730
+# @ http://stackoverflow.com/questions/8112208/how-can-i-obtain-an-unbalanced-grid-of-ggplots
+
+
+### include
+library(ggplot2)
+
+### data
+data(iris)
