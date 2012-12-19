54b3b4807ce8c3ee40f7956fe241da0950bb0e4d
variani <machovariani@gmail.com>
2012-12-19 01:33:31 +0100

    cart

--- cart/04-univar-reg.R
+++ cart/04-univar-reg.R
@@ -11,9 +11,11 @@ set <- c("A 0.001", "A 0.005", "A 0.01", "A 0.02", "A 0.05", "A 0.1")
 
 sa <- SensorArray(num = 3:5, dsd = 0)
 
-sc <- Scenario(T = rep(set, 20), V = rep(set, 10))
-conc <- getConc(sc)
-sdata <- predict(sa, conc, nclusters = 2)
+if(!exists("sc")) {
+  sc <- Scenario(T = rep(set, 20), V = rep(set, 10))
+  conc <- getConc(sc)
+  sdata <- predict(sa, conc, nclusters = 2)
+}
 
 p1 <- plotSignal(sa, set = set)
 p1
