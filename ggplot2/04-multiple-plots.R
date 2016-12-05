b4ba92e4909a863ed9319b1b5328ba4e5f5400f6
variani <machovariani@gmail.com>
2016-12-05 14:21:56 -0500

    multiple ggplots with custom layout

---
+++ ggplot2/04-multiple-plots.R
@@ -0,0 +1,10 @@
+library(ggplot2)
+library(gridExtra)
+
+p1 <- qplot(rnorm(100))
+p2 <- qplot(rchisq(100, 1))
+p3 <- qplot(rcauchy(100))
+
+layout <- rbind(c(1, 2), c(3, 3))
+
+grid.arrange(p1, p2, p3, layout_matrix = layout)
